77b4130de31dde2c823d196a7d88b52a4c460d85
nazimbo <113207575+nazimbo@users.noreply.github.com>
2024-04-30 12:17:21 +0200

    initial commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      header works!
+    </p>
+  `,
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
